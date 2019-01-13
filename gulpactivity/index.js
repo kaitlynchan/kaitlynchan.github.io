2791da08625bfd2493341c9405d79dd2ef5305ce
Kaitlyn Chan <kaitlynchan@Kaitlyns-MacBook-Pro-2.local>
2019-01-12 16:15:43 -0800

    sldkf

--- gulpactivity/index.js
+++ gulpactivity/index.js
@@ -4,26 +4,6 @@ $(document).ready(function () {
     $(this).toggleClass("flip");
     console.log('clicked')
   })
-    var keySequence = [
-      'ArrowUp',
-      'ArrowUp',
-      'ArrowDown',
-      'ArrowDown',
-      'ArrowLeft',
-      'ArrowRight',
-      'ArrowLeft',
-      'ArrowRight',
-      'b',
-      'a',
-  ];
-  var userInput = new Array(keySequence.length);
 
-  window.addEventListener( 'keydown', function( { key } ){
-    userInput = [ ...userInput.slice( 1 ), key ];
-
-    if ( keySequence.every( function( v, k ) { v === userInput[ k ] })) {
-        alert( 'Yatta!' );
-    }
-  } );
 
 });
