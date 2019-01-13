f221183163d1bb6d608fedd4d521fa81415b1c4a
Kaitlyn Chan <kaitlynchan@Kaitlyns-MacBook-Pro-2.local>
2019-01-12 18:33:25 -0800

    rename

---
+++ index.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function () {
+  $("div.flip-wrapper").click(function() {
+    $(this).toggleClass("flip");
+    console.log('clicked')
+  })
+
+  function showEgg(){
+    var egg = document.getElementsByClassName('egg')[0];
+    egg.className = '';
+  }
+  var code = "";
+  window.addEventListener("keydown", trackKeys);
+
+  function trackKeys(e){
+    code += e.key;
+    console.log(code);
+    if (code == 'totoro') {
+      $('#myModal').modal()
+      showEgg();
+      code = '';
+    }
+    else if (code.length > 6 && code.substring(code.length-6) == 'totoro'){
+      $('#myModal').modal()
+      showEgg();
+      code = '';
+    }
+  }
+
+});
